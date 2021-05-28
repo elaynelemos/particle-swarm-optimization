7c2025a8e1c94e1d07081ec1b9e6ab52eafe3d6c
elayne.lemos <elayne.lemos@sysvale.com>
2021-05-28 10:27:12 -0300

    Updates optimization function

--- particle_swarm_optimization/particle_swarm_optimization.py
+++ particle_swarm_optimization/particle_swarm_optimization.py
@@ -1,29 +1,22 @@
 from particle_swarm_optimization.models.particle import Particle
 from particle_swarm_optimization.models.swarm import Swarm
 
-def swarm_main():
-    W = 0.5
-    c1 = 0.8
-    c2 = 0.9
-
-    n_iterations = 50
-    target_error = 1e-6
-    n_particles = 30
-
-    search_space = Swarm(1, target_error, n_particles)
-    particles_vector = [Particle() for _ in range(search_space.n_particles)]
-    search_space.particles = particles_vector
-    search_space.print_particles()
+def optimize(target: float, target_error: float, iterations: int, swarm_size: int):
+    swarm = Swarm(target, target_error, swarm_size)
+    particles = [Particle() for _ in range(swarm.swarm_size)]
+    swarm.particles = particles
+    swarm.print_particles()
+    print()
 
     iteration = 0
-    while(iteration < n_iterations):
-        search_space.set_pbest()
-        search_space.set_gbest()
+    while iteration < iterations:
+        swarm.set_best_position()
 
-        if(abs(search_space.gbest_value - search_space.target) <= search_space.target_error):
+        if abs(swarm.best_value - swarm.target) <= swarm.target_error:
             break
 
-        search_space.move_particles()
+        swarm.move_particles()
         iteration += 1
 
-    print("The best solution is: ", search_space.gbest_position, " in n_iterations: ", iteration)
+    print(f'\nThe best position is: {swarm.best_position} = {swarm.best_value}'
+          f'\nReached in {iteration} iterations')
