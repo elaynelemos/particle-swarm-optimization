4825867c528a1170b63d59be7ace30a7a01e3ff3
elayne.lemos <elayne.lemos@sysvale.com>
2021-05-28 10:25:34 -0300

    Updates reference values

--- particle_swarm_optimization/helper.py
+++ particle_swarm_optimization/helper.py
@@ -2,6 +2,6 @@ import random
 import numpy as np
 
 
-def random_position(min=-3.0, max=3.0):
+def random_position(min=-5.0, max=5.0):
     pos = lambda : min + (max-min)*random.random()
     return np.array([pos(), pos()])
